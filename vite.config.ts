0521d0d4b4d50a1549ccd344e4d06ffbdf76d6f9
morizawa-tt <78008940+morizawa-tt@users.noreply.github.com>
2026-02-02 01:01:11 -0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [vue()],
-      base: '/ds-studio/',
+  base: '/ds-studio/',
+  resolve: {
+    alias: {
+      'src': resolve(__dirname, 'src'),
+    },
+  },
   server: {
     port: 5173,
     strictPort: true,
